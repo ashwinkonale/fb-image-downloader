aa29a412943c224fed2b45345f912dddd5fc9d9a
ashwinkonale <ashwin.konale@gmail.com>
2016-11-10 20:14:14 +0530

    download

--- image_download/image_download.py
+++ image_download/image_download.py
@@ -6,8 +6,8 @@ PAGINATION_URL = "https://graph.facebook.com/v2.8/1315007841865494?fields=photos
 ALL_IMAGE_LINKS_URL = "https://graph.facebook.com/v2.8/%s?fields=webp_images&access_token=EAACEdEose0cBAANQYB84n7wLZCPNGehXOi9ZCJfSK8zSBr0LCKjGGrSCojpCCkD0ISBPPwWiHbJPeI8F6DfC2X9001po5pn3cM4JXecbvefFNuLzG1XOk9n3r2bBvX2ou1zs3a3qNSEaqzOu8PLkaN1ZBktTZA9HhtsSafthCAZDZD"
 
 def download_image(url, img_id):
-    print img_id
-    # urllib.urlretrieve(url, "%s.jpg" % img_id)
+    print "Downloading %s"%image_id
+    urllib.urlretrieve(url, "%s.jpg" % img_id)
 
 
 def get_image_id(pagination_url, first = False):
@@ -28,16 +28,18 @@ def get_full_resolution_image_url(image_id):
     all_images = json.loads(data.read())
     full_length_image_url = all_images.get("webp_images")[0]["source"]
     full_length_image_url = full_length_image_url.replace(".webp", "")
-    print full_length_image_url
+    download_image(full_length_image_url,  image_id)
 
 if __name__ == "__main__":
     counter = 0
     image_id = ""
     next_image = ""
     image_id, next_image = get_image_id(PAGINATION_URL, True)
-    while True:
+    while next_image!=None:
         get_full_resolution_image_url(image_id)
         counter+=1
         print counter
         image_id, next_image = get_image_id(next_image)
 
+    get_full_resolution_image_url(image_id)
+    print "Thank you"
