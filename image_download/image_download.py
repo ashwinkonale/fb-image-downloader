4329d9976bb4b736017e8a5cb199236773783fca
ashwinkonale <ashwin.konale@gmail.com>
2016-11-10 20:07:18 +0530

    first

---
+++ image_download/image_download.py
@@ -0,0 +1,43 @@
+import json
+import urllib
+
+ALBUM_ID = "1315007841865494"
+PAGINATION_URL = "https://graph.facebook.com/v2.8/1315007841865494?fields=photos.limit(1)&access_token=EAACEdEose0cBAANQYB84n7wLZCPNGehXOi9ZCJfSK8zSBr0LCKjGGrSCojpCCkD0ISBPPwWiHbJPeI8F6DfC2X9001po5pn3cM4JXecbvefFNuLzG1XOk9n3r2bBvX2ou1zs3a3qNSEaqzOu8PLkaN1ZBktTZA9HhtsSafthCAZDZD"
+ALL_IMAGE_LINKS_URL = "https://graph.facebook.com/v2.8/%s?fields=webp_images&access_token=EAACEdEose0cBAANQYB84n7wLZCPNGehXOi9ZCJfSK8zSBr0LCKjGGrSCojpCCkD0ISBPPwWiHbJPeI8F6DfC2X9001po5pn3cM4JXecbvefFNuLzG1XOk9n3r2bBvX2ou1zs3a3qNSEaqzOu8PLkaN1ZBktTZA9HhtsSafthCAZDZD"
+
+def download_image(url, img_id):
+    print img_id
+    # urllib.urlretrieve(url, "%s.jpg" % img_id)
+
+
+def get_image_id(pagination_url, first = False):
+    data = urllib.urlopen(pagination_url)
+    image_data = json.loads(data.read())
+    if(first):
+        image_id = image_data.get("photos").get("data")[0]["id"]
+        next_image = image_data.get("photos").get("paging").get("next")
+    else:
+        image_id = image_data.get("data")[0]["id"]
+        next_image = image_data.get("paging").get("next")
+    print image_id
+    print next_image
+    return image_id, next_image
+
+def get_full_resolution_image_url(image_id):
+    data = urllib.urlopen(ALL_IMAGE_LINKS_URL%str(image_id))
+    all_images = json.loads(data.read())
+    full_length_image_url = all_images.get("webp_images")[0]["source"]
+    full_length_image_url = full_length_image_url.replace(".webp", "")
+    print full_length_image_url
+
+if __name__ == "__main__":
+    counter = 0
+    image_id = ""
+    next_image = ""
+    image_id, next_image = get_image_id(PAGINATION_URL, True)
+    while True:
+        get_full_resolution_image_url(image_id)
+        counter+=1
+        print counter
+        image_id, next_image = get_image_id(next_image)
+
